02211e9607b472afa3bce6c2468d1db48748ac07
zhangwenhao <yuerang.zwh@raycloud.com>
2018-07-30 17:50:59 +0800

    static

--- node/myExc/static-server/src/app.js
+++ node/myExc/static-server/src/app.js
@@ -3,30 +3,32 @@ const url = require('url');
 const path = require('path');
 const util = require('util');
 const fs = require('fs');
+const crypto = require('crypto');
 const zlib = require('zlib');
 const mime = require('mime'); // 内容类型
 const debug = require('debug')('*'); // 根据环境变量打印输出
 const chalk = require('chalk'); // 粉笔
-const ejs = require('ejs'); // underscore handlebar ejs jade....
+const swig = require('swig');
 // 1、指定主机名、端口号、静态文件存放目录
 const CONFIG = require('./config');
 
 const stat = util.promisify(fs.stat);
 const readdir = util.promisify(fs.readdir);
+const templateStr = swig.compileFile(path.join(__dirname, 'index.html'));
 
 class Server {
     constructor() {
         this.config = CONFIG;
+        this.templateStr = templateStr;
     }
     /**
-     * 
+     * 处理请求
      * @param {*} req
      * @param {*} res
      */
     async handleRequest(req, res) {
         const { pathname } = url.parse(req.url, true);
         const localPath = path.join(this.config.dir, pathname);
-        console.log('-----', localPath);
 
         if (pathname === '/favicon.ico') { // 如果是'/favicon.ico'不处理当前请求
             res.end();
@@ -37,15 +39,17 @@ class Server {
             const statObj = await stat(localPath);
             if (statObj.isDirectory()) { // 判断是否是文件夹
                 const dirs = await readdir(localPath);
-                const dirsObj = dirs.map(dir => {
+                const dirsArr = dirs.map(dir => {
                     return {
                         name: dir,
-                        path: path.join(this.config.dir, dir),
+                        path: path.join(pathname, dir),
                     }
                 });
-                console.log(dirsObj);
+                const tplStr = templateStr({ dirsArr });
+                res.setHeader('Content-Type', 'text/html;charset=utf-8');
+                res.end(tplStr);
             } else { // 不是文件夹就是文件
-                this.sendFile(req, res, localPath, stat);
+                this.sendFile(req, res, localPath, statObj);
             }
 
             // res.end(util.inspect(statObj.__proto__).toString());
@@ -56,12 +60,22 @@ class Server {
 
     /**
      * 
-     * @param {*} req 
-     * @param {*} res 
-     * @param {*} localPath 
-     * @param {*} stat 
+     * @param {Object} req 原生req
+     * @param {Object} res 原生res
+     * @param {String} localPath 当前请求地址对应文件的绝对路径
+     * @param {*} stat 当前文件的状态，缓存压缩等等功能用到
      */
-    sendFile(req, res, localPath, stat) {
+    async sendFile(req, res, localPath, stat) {
+        // 判断文件是否已经缓存，设置状态码
+        // this.needCache(req, res, localPath, stat);
+        if (await this.needCache(req, res, localPath, stat)) {
+            res.statusCode = 304;
+            res.end();
+            return;
+        }
+
+        res.setHeader('Content-type', mime.getType(localPath) + ';charset=utf-8');
+        const zip = this.compress(req, res, localPath, statObj);
         fs.createReadStream(localPath).pipe(res);
     }
 
@@ -78,8 +92,50 @@ class Server {
         res.end('NOT FOUND');
     }
 
+    async needCache(req, res, localPath, stat) {
+        res.setHeader('Cache-Control', 'no-cache');
+        res.setHeader('Expires', new Date(Date.now() + 10 * 1000).toGMTString());
+        const etag = await this.md5Encoding(localPath);
+        const lastModified = stat.ctime.toGMTString();
+
+        res.setHeader('Etag', etag);
+        res.setHeader('Last-Modified', lastModified);
+
+        const ifNoneMatch = req.headers['if-none-match'];
+        const ifModifiedSince = req.headers['if-modified-since'];
+        if (lastModified !== ifModifiedSince) return false;
+        if (etag !== ifNoneMatch) return false;
+        return true;
+    }
+
+    
+
     /**
-     * 
+     * 根据文件内容获得文件的 md5 编码
+     * @param  {String} path 
+     * @return {Promise}
+     */
+    md5Encoding(path) {
+        return new Promise(resolve => {
+            try {
+                const hasher = crypto.createHash('md5');
+    
+                const stream = fs.createReadStream(path);
+                stream.on('data', (currentData) => {
+                    hasher.update(currentData);
+                });
+                stream.on('end', () => {
+                    const code = hasher.digest('hex');
+                    resolve(code);
+                });
+            } catch(err) {
+                reject(err);
+            }
+        });
+    }
+
+    /**
+     * 启动服务方法
      */
     start() {
         const { hostname, port } = this.config;
@@ -88,6 +144,5 @@ class Server {
         debug(`server is listening on http://${chalk.yellow(hostname)}:${chalk.yellow(port)}`);
     }
 }
-
 const server = new Server();
 server.start();
