446432364a2e4cd555267309ab5fe8b4125da609
AlanWenhao <13454782783@163.com>
2019-08-29 17:18:15 +0800

    queryParser

--- node/tour/promise/queryParse.js
+++ node/tour/promise/queryParse.js
@@ -1,8 +1,10 @@
-const str = 'a=1&b=2&c=3';
+const str = '&aa=1&b=2&c=3';
 
 function queryParser(str) {
     let obj = {};
-    str.replace(/([^=&+])=([^=&+])/g, function() {
+    // 如果指定 /g 就意味着要被替换多次，那这个方法将会被调用多次，每次匹配都会被调用
+    str.replace(/[?&]([^=&#]+)=([^&#]*)/g, function() {
+        console.log('args', arguments);
         obj[arguments[1]] = arguments[2];
     })
     return obj
