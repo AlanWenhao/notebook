e6d3d52742156e90ddc60af533c2e137a3884d53
AlanWenhao <13454782783@163.com>
2020-04-13 17:31:58 +0800

    ts

--- typescript/playground.ts
+++ typescript/playground.ts
@@ -1,3 +1,15 @@
-function factory<T> (type: {new(): T}): T {
-    return new type()
+interface IAnyObj {
+    [prop: string]: any
+}
+
+function mixin<T extends IAnyObj, U extends IAnyObj>(a: T, b: U): T & U {
+    const result = <T & U>{};
+    for(let key in a) {
+        (<T>result)[key] = a[key];
+    }
+    for(let key in b) {
+        (<U>result)[key] = b[key];
+    }
+
+    return result;
 }
